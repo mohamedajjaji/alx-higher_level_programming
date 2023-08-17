01e117f1c6a0897ff698517fdd2efb2afaaa3e78
Mohamed AJJAJI <majjaji98@gmail.com>
2023-08-16 18:01:29 -0700

    Squared by using map - fixed

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    matrix_m = list(map(lambda x: list(map(lambda y: y**2, x)), matrix))
-    return (matrix_m)
+    return (list(map(lambda x: list(map(lambda y: y**2, x)), matrix)))
