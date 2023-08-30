ba0faeda863e2a5787507e84f201f15ab6a1e86a
Mohamed AJJAJI <majjaji98@gmail.com>
2023-08-29 17:04:42 -0700

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Class """
+
+
+class Square:
+    """ Class square with size """
+
+    def __init__(self, size):
+        """ Constructor of the Square """
+        self.__size = size
