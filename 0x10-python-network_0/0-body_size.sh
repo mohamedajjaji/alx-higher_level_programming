a2d337173d07afc98ca1b996ec32f203b3bbbfa6
Mohamed AJJAJI <majjaji98@gmail.com>
2023-11-30 13:40:24 -0800

    update

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Use curl to send a request to the URL
-# and store the response body size in bytes
-
+# Use curl to send a request to the URL, store the response body size in bytes
 curl -sI "$1" | awk '/Content-Length/{print $2}'
